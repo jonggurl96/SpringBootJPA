b6e0a2b84ac71c265ce82f6a9df88804b4ea4f80
jonggurl96 <jonggurl96@naver.com>
2023-01-31 14:10:17 +0900

    update data.sql add init data

--- JPAdemo/src/main/resources/data.sql
+++ JPAdemo/src/main/resources/data.sql
@@ -3,3 +3,12 @@ values (1, 'Do it! HTML5 + CSS3 웹 표준의 정석');
 
 insert into book (id, title)
 values (2, '코드로 배우는 스프링 웹 프로젝트');
+
+insert into book (id, title)
+values (3, 'JUSTICE, 정의란 무엇인가?');
+
+insert into book_store (id, name)
+values (1, 'yes24');
+
+insert into book_store (id, name)
+values (2, '교보문고');
